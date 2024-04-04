c020a79bda22f90845cea4353898af16e0d6dd2b
Anthony Frassino <tonyfrin@gmail.com>
2024-04-03 21:13:04 -0400

    Version 0.1.03 Prueba

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({ subsets: ["latin"], weight: ['300', '400', '500', '600', '700', '800', '900']});
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={`${poppins.className}`}>
+      <body>
         <Main />
         <NextScript />
       </body>
