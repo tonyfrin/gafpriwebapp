e043187c52096b91181d86b6b23d35eb8381cc45
Anthony Frassino <tonyfrin@gmail.com>
2024-04-03 22:44:29 -0400

    Version 0.1.09 WalletSetion

--- src/Abstract/Component/ServiceHome.tsx
+++ src/Abstract/Component/ServiceHome.tsx
@@ -3,6 +3,7 @@ import { Layout } from '../../Abstract/Component/Layout';
 import { VideoSection } from "../../Abstract/Section/VideoSection";
 import { ServiceSection } from "../../Abstract/Section/ServiceSection";
 import { StoreSection } from "../../Abstract/Section/StoreSection";
+import { WalletSection } from "../../Abstract/Section/WalletSection";
 
 
 export const ServiceHome = (): JSX.Element => {
@@ -13,8 +14,8 @@ export const ServiceHome = (): JSX.Element => {
                    <VideoSection/>
                    <ServiceSection/>
                    <StoreSection/>
-                   {/* <WalletSection />
-                   <DeliverySection/>
+                   <WalletSection />
+                   {/* <DeliverySection/>
                    <CreditSection /> */}
                 </>
             </Layout>
---
+++ src/Abstract/Section/WalletSection.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { css } from '@emotion/css';
+import Image from "next/image";
+import WalletBanner from '../assets/img/fondo-wallet.jpg';
+
+const serviceSectionContainer = css`
+    width: 100%;
+    margin-top: 10rem;
+    margin-bottom: 2rem;
+`;
+
+const serviceSectionContent = css`
+    grid-auto-columns: 1fr;
+    display: grid;
+    grid-row-gap: 16px;
+    grid-column-gap: 16px;
+    grid-template-rows: auto;
+    grid-template-columns: 1.5fr 1.5fr;
+    justify-items: stretch;
+    margin-top: 1rem;
+`
+
+const containerText = css`
+    grid-area: span 1 / span 1 / span 1 / span 1;
+    align-self: center;
+    justify-self: start;
+    padding: 1rem 4rem;
+    box-sizing: border-box;
+`
+
+const titleStyle = css`
+    opacity: 1;
+    transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+    transform-style: preserve-3d;
+    text-align: left;
+    font-size: 2.2rem;
+    font-weight: 700;
+    line-height: 1.2;
+`
+
+const serviceSectionContentSmallTitle = css`
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    color: #1f1f1f;
+    font-family: Poppins, sans-serif;
+    font-weight: 400;
+`
+
+const serviceSectionContentImage = css`
+    opacity: 1;
+    transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+    transform-style: preserve-3d;
+    grid-area: span 1 / span 1 / span 1 / span 1;
+    border-radius: 33px;
+    padding-right: 0;
+    max-width: 100%;
+    display: inline-block;
+`
+
+export const WalletSection = () => {
+    return (
+        <section className={serviceSectionContainer}>
+            <div className={serviceSectionContent}>
+                <div className={containerText}>
+                    <h3 className={titleStyle}>Recibe pagos de tus clientes de manera segura y retira tus fondos con tranquilidad.</h3>
+                    <p className={serviceSectionContentSmallTitle}>Recibe pagos en Zelle, Paypal o Transferencias y retira efectivo o compra el repuesto que necesites online o en cualquiera de nuestras tiendas asociadas</p>
+                </div>
+                <Image width={WalletBanner.width} height={WalletBanner.height} className={serviceSectionContentImage} src={WalletBanner.src} alt="Wallet" />
+            </div>
+        </section>
+    );
+}
