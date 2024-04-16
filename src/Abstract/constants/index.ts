cbf8aed2025d22a45b92fd21b04ca05dc216d5b2
Anthony Frassino <tonyfrin@gmail.com>
2024-04-16 15:43:32 -0400

    Version 0.1.23 conexion api image

--- src/Abstract/constants/index.ts
+++ src/Abstract/constants/index.ts
@@ -17,5 +17,5 @@ export const WALLET_ACCOUNT_ROUTE = '/wallet-account';
 export const PAYMENT_WALLET = '/payment-wallet';
 export const WALLET_TRANSACTIONS = '/wallet-transactions';
 export const EMAIL_CHECK_ROUTE = '/email-check';
-export const UPLOAD_PHOTO_ROUTE = `${getApiUrl()}/upload-file`;
+export const UPLOAD_PHOTO_ROUTE = `${getApiUrl()}//upload-file`;
 export const TYPE_DOCUMENT_ID_ROUTE = '/type-document-id';
